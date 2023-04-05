8a84a7b2799fdcfa3b5a2646031fb34fe871820f
Bugheanu Danut <bugheanudanutandrei@gmail.com>
2023-04-05 13:42:32 +0300

    optimize the styling by creating a separate folder with the styles

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const dir = require("./utility/path");
-
 const app = express();
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
