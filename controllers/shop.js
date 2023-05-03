c83b7320edf4bfeb37fb4b4414c8cd12625b8e20
Bugheanu Danut <bugheanudanutandrei@gmail.com>
2023-05-03 14:09:46 +0300

    adding the routes for specific files

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,11 +1,35 @@
 const Product = require("../models/products");
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll((products) => {
+  Product.fetchAll((products) => {
     res.render("shop/product-list", {
+      prod: products,
+      pageTitle: "All products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
       prod: products,
       pageTitle: "Shopy",
       path: "/",
     });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    pageTitle: "Your Cart",
+    path: "/cart",
+  });
+};
+
+exports.getCheckout = (res, req, next) => {
+  res.render("shop/checkout", {
+    pageTitle: "Checkout",
+    path: "/checkout",
   });
 };
