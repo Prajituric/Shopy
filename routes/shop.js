5e73569d600cc7b5a60791e9f54678bc3601f096
Bugheanu Danut <bugheanudanutandrei@gmail.com>
2023-05-04 14:08:14 +0300

    adding the details functionality

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,13 @@ const router = express.Router();
 router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
+
+router.get("/products/:productID", shopController.getProduct);
+
 router.get("/cart", shopController.getCart);
+
 router.get("/orders", shopController.getOrders);
+
 router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
