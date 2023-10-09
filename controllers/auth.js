5e52aa4c0eb8f0373a33e9980900601eade7ee35
Bugheanu Danut <bugheanudanutandrei@gmail.com>
2023-10-09 11:39:10 +0300

    sending emails

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require("bcryptjs");
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
+const mg = new Mailgun(formData);
 
 const User = require("../models/user");
+
+const mailgunClient = mg.client({
+  username: "api",
+  key: "60c82056ccc33238e548f63ceac3331e-5465e583-9492a53f",
+});
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -65,17 +73,19 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  let mailgunSuccess = false; // Declare mailgunSuccess variable here
 
-  if (!password || !confirmPassword) {
+  // Input validation code
+  if (
+    !password ||
+    !confirmPassword ||
+    password.length < 8 ||
+    confirmPassword.length < 8
+  ) {
     req.flash(
       "error",
-      "You need to add both your password and confirm your password."
+      "Invalid password. Password must have at least 8 characters."
     );
-    return res.redirect("/signup");
-  }
-
-  if (password.length < 8 || confirmPassword.length < 8) {
-    req.flash("error", "Password must have at least 8 characters.");
     return res.redirect("/signup");
   }
 
@@ -86,10 +96,7 @@ exports.postSignup = (req, res, next) => {
   }
 
   if (password !== confirmPassword) {
-    req.flash(
-      "error",
-      "Passwords do not match. Please ensure they are the same."
-    );
+    req.flash("error", "Passwords do not match.");
     return res.redirect("/signup");
   }
 
@@ -99,6 +106,7 @@ exports.postSignup = (req, res, next) => {
         req.flash("error", "Your e-mail is already in use.");
         return res.redirect("/signup");
       }
+
       return bcrypt
         .hash(password, 12)
         .then((hashedPassword) => {
@@ -110,7 +118,39 @@ exports.postSignup = (req, res, next) => {
           return user.save();
         })
         .then((result) => {
-          res.redirect("/login");
+          // Send email using Mailgun
+          const mailgunData = {
+            from: "Excited User <mailgun@sandboxa498eebb2fbd488c99b945b413e91081.mailgun.org>",
+            to: [email],
+            subject: "Signup succeeded!",
+            text: "You successfully signed up!",
+            html: "<h1>You successfully signed up!</h1>",
+          };
+
+          return mailgunClient.messages
+            .create(
+              "sandboxa498eebb2fbd488c99b945b413e91081.mailgun.org",
+              mailgunData
+            )
+            .then((msg) => {
+              console.log(msg);
+              // Set mailgunSuccess to true on success
+              mailgunSuccess = true;
+              return Promise.resolve(); // Resolve the promise
+            })
+            .catch((err) => {
+              console.log(err);
+              // Handle the error, e.g., log it or perform other actions
+              return Promise.reject(err); // Reject the promise on error
+            });
+        })
+        .then(() => {
+          // After the Mailgun promise is resolved, perform the redirection
+          if (mailgunSuccess) {
+            res.redirect("/login"); // Redirect to login on success
+          } else {
+            res.redirect("/signup"); // Redirect to signup on failure
+          }
         });
     })
     .catch((err) => {
